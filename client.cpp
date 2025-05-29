571e3a894b8b870d31540517348f2186e5428842
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2025-05-29 20:19:13 +0100

    client is ready

--- client.cpp
+++ client.cpp
@@ -4,8 +4,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int main(int c, char *av[])
+int main(int ac, char *av[])
 {
+    if (ac != 2)
+    {
+        perror("Try with a valid input!");
+        exit (EXIT_FAILURE);
+    }
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket < 0)
     {
@@ -18,12 +23,16 @@ int main(int c, char *av[])
     server_address.sin_port = htons(8080);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    connect(client_socket, (sockaddr *)&server_address, sizeof(server_address));
-    
-    // char buff[1024];
-    // memset(&buff , 0 , sizeof(buff));
+    if (connect(client_socket, (sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
+        perror("connection failed!");
+        exit (EXIT_FAILURE);
+    }
     const char *message = av[1];
-
-    send(client_socket, message, strlen(message), 0);
+    if (send(client_socket, message, strlen(message), 0) == -1)
+    {
+        perror("message doesen't sent!");
+        exit (EXIT_FAILURE);
+    }
     close (client_socket);
 }
